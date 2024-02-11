782c41496076835a9ddfe8359a3c5fd3fde3c7b1
Yahia <yn7599@gmail.com>
2024-02-11 15:30:37 +0200

    day 1 done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SliderComponent } from './slider/slider.component';
+import { NavComponent } from './nav/nav.component';
+import { AboutComponent } from './about/about.component';
+import { CardComponent } from './products/card/card.component';
+import { CardListComponent } from './products/card-list/card-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SliderComponent,
+    NavComponent,
+    AboutComponent,
+    CardComponent,
+    CardListComponent
   ],
   imports: [
     BrowserModule,
